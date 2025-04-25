a9062a36217fef47e4103d375eb066d01a48084c
hb <gksqls9735@gmail.com>
2025-04-25 17:20:13 +0900

    feat: Add 'Add Todo' button to TodoList

--- src/component/kanban/card-todo/todolist.tsx
+++ src/component/kanban/card-todo/todolist.tsx
@@ -29,27 +29,27 @@ const TodoList: React.FC<{
     updateTask(taskId, { 'todoList': newTodoList });
   };
 
-    const sensors = useSensors(
-      useSensor(PointerSensor, {
-        activationConstraint: {
-          distance: 8,
-        },
-      }),
-      useSensor(KeyboardSensor)
-    );
-  
-    const handleTodoCompleteChange = (todoId: string) => {
-      const updatedTodoList = todoList.map(items => {
-        if (items.todoId === todoId) return { ...items, isCompleted: !items.isCompleted };
-        return items;
-      });
-      updateTask(taskId, { 'todoList': updatedTodoList });
-    };
-  
-    const handleDeleteTodo = (todoId: string) => {
-      const updatedTodoList = todoList.filter(todo => todo.todoId !== todoId);
-      updateTask(taskId, { 'todoList': updatedTodoList });
-    };
+  const sensors = useSensors(
+    useSensor(PointerSensor, {
+      activationConstraint: {
+        distance: 8,
+      },
+    }),
+    useSensor(KeyboardSensor)
+  );
+
+  const handleTodoCompleteChange = (todoId: string) => {
+    const updatedTodoList = todoList.map(items => {
+      if (items.todoId === todoId) return { ...items, isCompleted: !items.isCompleted };
+      return items;
+    });
+    updateTask(taskId, { 'todoList': updatedTodoList });
+  };
+
+  const handleDeleteTodo = (todoId: string) => {
+    const updatedTodoList = todoList.filter(todo => todo.todoId !== todoId);
+    updateTask(taskId, { 'todoList': updatedTodoList });
+  };
 
   return (
     <div className="card-todolist">
@@ -65,13 +65,23 @@ const TodoList: React.FC<{
         onDragEnd={handleDragEnd}
         modifiers={[restrictToVerticalAxis, restrictToFirstScrollableAncestor]}
       >
-        <SortableContext items={todoList.map(todo => todo.todoId)} strategy={verticalListSortingStrategy}>
-          <div className={`todo-list ${showTodo ? 'todo-list--open' : ''}`}>
+        <div className={`todo-list ${showTodo ? 'todo-list--open' : ''}`}>
+          <SortableContext items={todoList.map(todo => todo.todoId)} strategy={verticalListSortingStrategy}>
             {todoList.map(todo => (
               <DraggableTodo key={todo.todoId} todo={todo} onCompleteChange={handleTodoCompleteChange} onDelete={handleDeleteTodo} />
             ))}
+          </SortableContext>
+          <div className="card-add-todo">
+            <div>
+              <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="#7D8998" className="bi bi-plus-lg" viewBox="0 0 16 16">
+                <path fillRule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2" />
+              </svg>
+            </div>
+            <div className="card-add-todo__text">
+              할 일 추가
+            </div>
           </div>
-        </SortableContext>
+        </div>
       </DndContext>
     </div>
   );
