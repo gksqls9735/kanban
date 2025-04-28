bf5e43d6212a91dcd048302ddedfcb14a92e161f
hb <gksqls9735@gmail.com>
2025-04-28 16:25:37 +0900

    feat: Require edit mode for task modification and remove inline Todo add UI

--- src/component/kanban/card-todo/todolist.tsx
+++ src/component/kanban/card-todo/todolist.tsx
@@ -71,16 +71,6 @@ const TodoList: React.FC<{
               <DraggableTodo key={todo.todoId} todo={todo} onCompleteChange={handleTodoCompleteChange} onDelete={handleDeleteTodo} />
             ))}
           </SortableContext>
-          <div className="card-add-todo">
-            <div>
-              <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="#7D8998" className="bi bi-plus-lg" viewBox="0 0 16 16">
-                <path fillRule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2" />
-              </svg>
-            </div>
-            <div className="card-add-todo__text">
-              할 일 추가
-            </div>
-          </div>
         </div>
       </DndContext>
     </div>
