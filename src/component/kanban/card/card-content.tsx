19e044d1a75c0518ea21bf6bd2b7b5750c65a63e
hb <gksqls9735@gmail.com>
2025-04-29 08:39:33 +0900

    style: Update CardContent component modal style

--- src/component/kanban/card/card-content.tsx
+++ src/component/kanban/card/card-content.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import useTaskStore from "../../../store/task-store";
 import { Task } from "../../../types/type";
 import useDropdown from "../../../hooks/use-dropdown";
@@ -13,6 +13,7 @@ const CardContent: React.FC<{
   onClick: () => void;
 }> = ({ task, sectionName, onClick }) => {
   const { isOpen, setIsOpen, wrapperRef, dropdownRef, toggle } = useDropdown();
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState<boolean>(false);
   const deleteTask = useTaskStore(state => state.deletTask);
   const copyTask = useTaskStore(state => state.copyTask);
 
@@ -24,7 +25,6 @@ const CardContent: React.FC<{
 
   const handleDelete = (e: React.MouseEvent) => {
     e.stopPropagation();
-    console.log("Delete Task: ", task.taskId);
     deleteTask(task.taskId);
     setIsOpen(false);
   };
@@ -54,7 +54,7 @@ const CardContent: React.FC<{
               </svg>
               작업 복사
             </div>
-            <div className="dropdown-item" onClick={handleDelete}>
+            <div className="dropdown-item" onClick={() => setIsDeleteModalOpen(true)}>
               <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi bi-trash3" viewBox="0 0 16 16">
                 <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
               </svg>
@@ -89,6 +89,23 @@ const CardContent: React.FC<{
           <div className="seperation-line" />
           <TodoList taskId={task.taskId} todoList={task.todoList} />
         </>
+      )}
+      {isDeleteModalOpen && (
+        <div className="delete-modal">
+          <div className="delete-modal__icon-container">
+            <svg xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px" fill="#D92D20">
+              <path d="M440-400v-360h80v360h-80Zm0 200v-80h80v80h-80Z" />
+            </svg>
+          </div>
+          <div className="delete-modal__message" >작업을 삭제하시겠습니까?</div>
+          <div className="delete-modal__actions">
+            <button
+              className="delete-modal__button delete-modal__button--cancel" onClick={() => setIsDeleteModalOpen(false)}>
+              취소</button>
+            <button
+              className="delete-modal__button delete-modal__button--confirm" onClick={handleDelete}>확인</button>
+          </div>
+        </div >
       )}
     </>
   );
