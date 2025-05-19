32803df21a9566dc38e97b863175a31762c75b22
hb <gksqls9735@gmail.com>
2025-05-19 14:06:37 +0900

    fix(DetailView): Apply flex-shrink: 0 to avatars to prevent shrinking

--- src/component/avatar/avatar.tsx
+++ src/component/avatar/avatar.tsx
@@ -50,7 +50,7 @@ const AvatarItem: React.FC<AvatarItemProps> = ({
       height={size}
       viewBox={`0 0 ${size} ${size}`}
       xmlns="http://www.w3.org/2000/svg"
-      style={{ display: 'block', marginLeft: `${isFirst ? -6 : 0}` }}
+      style={{ display: 'block', marginLeft: `${isFirst ? -6 : 0}`, flexShrink: 0 }}
     >
       <defs>
         <clipPath id={clipPathId}>
