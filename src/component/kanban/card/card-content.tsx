0ac381d59d6d1244a185de2ed08b32736daf601c
hb <gksqls9735@gmail.com>
2025-07-15 17:18:57 +0900

    Refactor: Relax update validation for Task `end` property to allow null values

--- src/component/kanban/card/card-content.tsx
+++ src/component/kanban/card/card-content.tsx
@@ -22,7 +22,8 @@ const CardContent: React.FC<{
         <svg xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 -960 960 960" width="16px" fill="#7d8998">
           <path d="M360-300q-42 0-71-29t-29-71q0-42 29-71t71-29q42 0 71 29t29 71q0 42-29 71t-71 29ZM200-80q-33 0-56.5-23.5T120-160v-560q0-33 23.5-56.5T200-800h40v-80h80v80h320v-80h80v80h40q33 0 56.5 23.5T840-720v560q0 33-23.5 56.5T760-80H200Zm0-80h560v-400H200v400Zm0-480h560v-80H200v80Zm0 0v-80 80Z" />
         </svg>
-        {formatDateToYyyyMmDd(task.start)} - {formatDateToYyyyMmDd(task.end)}</div>
+        {formatDateToYyyyMmDd(task.start)} {task.end ? ` - ${formatDateToYyyyMmDd(task.end)}` : ' (종료일 미정)'}
+      </div>
       <div className="card-meta">
         <CardMeta taskPriority={task.priority} taskStatus={task.status} />
         <CardParticipants taskParticipants={task.participants} onModalStateChange={onModalStateChange} />
